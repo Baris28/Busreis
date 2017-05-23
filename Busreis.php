09838a57b2109edfddf1cb3cb97d43b4e6a20c00
Baris28 <bbekdemir@roc-dev.com>
2017-05-23 13:25:48 +0200

    first commit

---
+++ Busreis.php
@@ -0,0 +1,13 @@
+<?php
+
+    $leeftijd = 12;
+
+        if($leeftijd >= 65) {
+         echo   "het kost 10 euro";
+        }
+        else if($leeftijd >= 10) {
+          echo  "het is 5 euro";
+        }
+        else {
+            echo "Het kost niks";
+        }
